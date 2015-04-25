25e409146f60860ecbcfa331c8aeafa2d9d11684
kellysc2013 <kellysc2013@users.noreply.github.com>
2015-04-25 16:05:16 -0300

    Update artigo_postgresql_regexp_replace_cpf.sql

--- artigo_postgresql_regexp_replace_cpf/artigo_postgresql_regexp_replace_cpf.sql
+++ artigo_postgresql_regexp_replace_cpf/artigo_postgresql_regexp_replace_cpf.sql
@@ -46,5 +46,5 @@ FROM tb_alunos;
  *pela expressão abreviada "\d". O resultado será o mesmo.
  */
 SELECT  
-REGEXP_REPLACE (cpf,  '(\d{3})(\d{3})(\d{3})(\d{2})',                 '\1.\2.\3-\4 ')   AS cpf 
+REGEXP_REPLACE (cpf,  '(\d{3})(\d{3})(\d{3})(\d{2})',  '\1.\2.\3-\4 ') AS cpf 
 FROM tb_alunos
