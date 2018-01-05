e1676feb78bccd8a65270ae9b846880f011cf804
kellysc2013 <kellysc2013@users.noreply.github.com>
2018-01-05 11:57:41 -0200

    Add files via upload

---
+++ artigo_copy_to/exemplo_2/tabela_estoque_inserir_registros.sql
@@ -0,0 +1,14 @@
+/*Comando para inserir multiplos registros na tabela estoque*/
+INSERT INTO estoque
+(
+    codigo,
+    produto,
+    categoria,
+    marca,
+    preco,
+    quantidade
+)
+VALUES
+(1, 'Smartphone Galaxy J7 Prime', 'Celular', 'Samsung', 869.99, 20),
+(2, 'ASUS ZenPad 3S 10 Z500M Tablet PC', 'Tablet', 'Asus', 1533.20, 27),
+(3, 'Vaio FIT 15 F', 'Notebook', 'Sony', 2999.00, 50);
