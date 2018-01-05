5bd60affa4225dfc858404ae671e88d6540f0a3b
kellysc2013 <kellysc2013@users.noreply.github.com>
2018-01-05 10:08:28 -0200

    Add files via upload

---
+++ artigo_copy_to/exemplo_1/create_script_tabela_estoque.sql
@@ -0,0 +1,9 @@
+CREATE TABLE estoque
+(
+    codigo integer,
+    produto character varying(100) COLLATE pg_catalog."default",
+    categoria character varying(100) COLLATE pg_catalog."default",
+    marca character varying(100) COLLATE pg_catalog."default",
+    preco numeric(10, 2),
+    quantidade integer
+);
