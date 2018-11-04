276e8774c57fb0b91e569706f17eee72ed3cc087
kellysc2013 <kellysc2013@users.noreply.github.com>
2018-11-04 11:15:36 -0200

    Update postgresql - converter varchar para data.sql
    PostgreSQL TO_DATE Function
    *Convert String to Date

--- postgresql - converter varchar para data.sql
+++ postgresql - converter varchar para data.sql
@@ -1,4 +1,4 @@
-﻿--Converter varchar para data - 1º EXEMPLO
+--Converter varchar para data - 1º EXEMPLO
 SELECT to_date('02/11/2018', 'DD/MM/YYYY') AS DATA
 
 --Converter varchar para data - 2º EXEMPLO
@@ -8,4 +8,4 @@ SELECT to_date('2018-11-02', 'YYYY-MM-DD') AS DATA
 SELECT to_date('02112018', 'DDMMYYYY') AS DATA
 
 --Converter varchar para data - 4º EXEMPLO
-SELECT to_date('02112018', 'DDMMYYYY') AS DATA
+SELECT to_date('20181102', 'YYYYMMDD') AS DATA
