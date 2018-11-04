4460f8fb6e8ec5d91b15a077fa57a25cf04c2526
kellysc2013 <kellysc2013@users.noreply.github.com>
2018-11-04 09:32:05 -0200

    Converter varchar para timestamp

--- converter varchar campo texto para timestamp.sql
+++ converter varchar campo texto para timestamp.sql
@@ -1,5 +1,5 @@
-﻿--SEM TIME ZONE (sem fuso horário)
+﻿--Converter varchar para timestamp SEM TIME ZONE (sem fuso horário)
 SELECT to_timestamp('2018-11-02 14:35:43', 'YYYY-MM-DD hh24:mi:ss') ::timestamp without time zone
 
---COM TIME ZONE (fuso horário)
+--Converter varchar para timestamp COM TIME ZONE (fuso horário)
 SELECT to_timestamp('2018-11-02 14:35:43', 'YYYY-MM-DD hh24:mi:ss')
